e70517a2be769bedc65a129b2ebf902f5f0376b8
Maxim Sytnyk <iaroki@protonmail.com>
2022-03-15 16:22:58 +0200

    add ssh outputs

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "ssh_key_id" {
+  value = module.ssh.ssh_key_id
+}
+
+output "ssh_key_name" {
+  value = module.ssh.ssh_key_name
+}
+
+output "ssh_key_fingerprint" {
+  value = module.ssh.ssh_key_fingerprint
+}
+
+output "ssh_key_labels" {
+  value = module.ssh.ssh_key_labels
+}
