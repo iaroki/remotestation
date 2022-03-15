78b9e237cad7d052ed9f2483ff73685c6f75b95a
Maxim Sytnyk <iaroki@protonmail.com>
2022-03-15 18:00:39 +0200

    add vars types

--- modules/hcloud_ssh_key/variables.tf
+++ modules/hcloud_ssh_key/variables.tf
@@ -1,11 +1,14 @@
 variable "hcloud_token" {
+  type = string
   sensitive = true
 }
 
 variable "ssh_pub_key_file" {
+  type = string
   default = "~/.ssh/id_rsa.pub"
 }
 
 variable "ssh_pub_key_name" {
+  type = string
   default = "terraform"
 }
--- variables.tf
+++ variables.tf
@@ -1,35 +1,44 @@
 variable "hcloud_token" {
+  type = string
   sensitive = true
 }
 
 variable "ssh_pub_key_name" {
+  type = string
   default = "msytnyk"
 }
 
 variable "server_name" {
+  type = string
   default = "terraform"
 }
 
 variable "server_image" {
+  type = string
   default = "debian-11"
 }
 
 variable "server_type" {
+  type = string
   default = "cx11"
 }
 
 variable "server_location" {
+  type = string
   default = "fsn1"
 }
 
 variable "server_ssh_key" {
+  type = string
   default = ""
 }
 
 variable "server_user_data" {
+  type = string
   default = null
 }
 
 variable "server_iso" {
+  type = string
   default = "11311" # NixOS 21.11 (amd64/minimal)
 }
