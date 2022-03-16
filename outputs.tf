5607c671111e41f604887c55db1bee885877da42
Maxim Sytnyk <iaroki@protonmail.com>
2022-03-16 11:58:18 +0200

    add server outputs

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,47 @@ output "ssh_key_fingerprint" {
 output "ssh_key_labels" {
   value = module.ssh.ssh_key_labels
 }
+
+output "server_id" {
+  value = module.server.server_id
+}
+
+output "server_name" {
+  value = module.server.server_name
+}
+
+output "server_type" {
+  value = module.server.server_type
+}
+
+output "server_image" {
+  value = module.server.server_image
+}
+
+output "server_location" {
+  value = module.server.server_location
+}
+
+output "server_datacenter" {
+  value = module.server.server_datacenter
+}
+
+output "server_iso" {
+  value = module.server.server_iso
+}
+
+output "server_ipv4_address" {
+  value = module.server.server_ipv4_address
+}
+
+output "server_ipv6_address" {
+  value = module.server.server_ipv6_address
+}
+
+output "server_status" {
+  value = module.server.server_status
+}
+
+output "server_labels" {
+  value = module.server.server_labels
+}
